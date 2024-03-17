09f33869a5f17502a9a06231b5b2d3d8d74ef764
Bhaskar <bhaskarkumar.india@gmail.com>
2024-03-18 02:09:15 +0530

    user input notes funtionality added! So proceeding for the last delete funtionality-->

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,58 @@ import Header from "../src/Header/Header"
 import Input from "./Input/Input"
 import Notes from "./Notes/Notes"
 import  './App.css'
-import data from "./Dummy"
+// import data from "./Dummy"
+import React,{useState} from "react"
 function App() {
+  let [data,setData]=useState([])
+
+  let[notes,setNotes]=useState({
+      title:"",
+      text:"",
+      color:"green"
+  })
+
+let handleChange=(e)=>{
+  // console.log(e.target.value, e.target.name)
+  setNotes((prevObj)=>({
+      ...prevObj,[e.target.name]:e.target.value
+}))
+
+}
+
+let handleColor=(e)=>{
+  setNotes((prevObj)=>({
+      ...prevObj,color:e.target.value
+  }))
+}
+
+
+let handleSubmit=()=>{
+  console.log(notes)
+if(notes.title!=0 && notes.text!=0){
+  setData((prevData)=>([
+      ...prevData,notes
+  ]))
+  console.log(data)
+  setNotes(()=>({
+  title:"",
+  text:""
+  }))
+}
+
+  }
 return(
 <>
 <div className="container">
 <Header/>
-<Input/>
+<Input   
+title={notes.title}
+text={notes.text}
+handleSubmit={handleSubmit}
+handleColor={handleColor}
+handleChange={handleChange}
+
+/>
 <div className="notesbox">
   {
     data.map((note,index)=>(
--- src/Input/Input.jsx
+++ src/Input/Input.jsx
@@ -1,20 +1,31 @@
 import styles from "./Input.module.css"
+import React,{useState} from "react"
+const Input=({title,text,handleChange,handleColor,handleSubmit})=>{
 
-const Input=()=>{
+    
+    
     return(
         <div className={styles.container}>
 
             <div className={styles.textbox}>
-                <input type="text" placeholder="Title" />
+                <input type="text" name="title" value={title} onChange={(e)=>handleChange(e)} placeholder="Title" />
                 <br />
                 
-                <textarea name="" id="" cols="30" rows="10" placeholder="Take a Note..."></textarea>
-                <button className={styles.btn}>+</button>
+                <textarea  onChange={(e)=>handleChange(e)} name="text" value={text} id="" cols="30" rows="10" placeholder="Take a Note..."></textarea>
+                <button onClick={(e)=>handleSubmit(e)} className={styles.btn}>+</button>
+                <div className={styles.colorpallete}>
+                <button onClick={(e)=>handleColor(e)} value="orange" style={{backgroundColor:"orange"}}></button>
+                <button onClick={(e)=>handleColor(e)} value="purple" style={{backgroundColor:"purple"}}></button>
+                <button onClick={(e)=>handleColor(e)} value="red" style={{backgroundColor:"red"}}></button>
+
+                <button onClick={(e)=>handleColor(e)} value="brown" style={{backgroundColor:"brown"}}></button>
+                <button onClick={(e)=>handleColor(e)} value="green" style={{backgroundColor:"green"}}></button>
+                <button onClick={(e)=>handleColor(e)} value="blue" style={{backgroundColor:"blue"}}></button>
                 
-            </div>
-            <div className={styles.colorpallete}>
-                    {/* <h1>Hello</h1> */}
+
                 </div>
+            </div>
+   
         </div>
 
     )
